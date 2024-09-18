30b33657eaeed749c02bf9b8cf1e49c5b0eceac4
Juliet Ugoh <Julietteugoh@gmail.com>
2024-09-18 08:40:55 +0100

    form validation that wont work even though it is correct

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const form = document.querySelector('#form');
+const Name = document.querySelector('#Name');
+const EmailAddress = document.querySelector('#EmailAddress');
+const Subject = document.querySelector('#Subject');
+const TextArea = document.querySelector('#TextArea');
+
+const nameError =  document.querySelector('.nameError');
+const emailError =  document.querySelector('.emailError');
+const SubjectError =  document.querySelector('.SubjectError');
+const TextAreaError =  document.querySelector('.TextAreaError');
+
+form.addEventListener('submit', (e) => {
+  if (Name.value === '' || Name.value == null) 
+      { e.preventDefault(); nameError.innerHTML = 'Kindly enter your Name!'; }
+
+  if (EmailAddress.value === '' || EmailAddress.value == null) 
+    { e.preventDefault(); emailError.innerHTML = 'Kindly enter your Email Address!'; }
+
+  if (Subject.value === '' || Subject.value == null) 
+    { e.preventDefault(); SubjectError.innerHTML = 'Kindly enter a subject!'; }
+
+
+  if (TextArea.value === '' || TextArea.value == null) 
+    { e.preventDefault(); TextAreaError.innerHTML = 'Type your complaint or request here'; }
+
+});
+
+
+
+
+
+const check = document.querySelector('.bodyshell1');
+
+check.addEventListener('mouseover', ()=> {check.style.backgroundColor ='green';});
